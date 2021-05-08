1742c95ef67fce435cb5e408280936bdbd9b08c3
102550264Brathaban <69609273+102550264Brathaban@users.noreply.github.com>
2021-05-08 23:30:23 +1000

    great css animation
     That was a nice design created by James in CSS. great animation. I added the javascript file so buttons functional.

---
+++ Front Page/Brath- FRONT PAGE/Front Page/script.js
@@ -0,0 +1,19 @@
+function button1_click(){
+window.open("addsales.html");
+//alert("hai");
+}
+
+function button2_click()
+{
+ window.open("salesrecord.html");
+}
+
+function button3_click()
+{
+window.open("stocklist.html");
+}
+
+function button4_click()
+{
+window.open("viewsalesreport.html");
+}
