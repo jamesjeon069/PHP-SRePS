9dc097791220d29079f5153bfa20fa5b487a5701
102550264Brathaban <69609273+102550264Brathaban@users.noreply.github.com>
2021-05-09 22:19:03 +1000

    latest javascript and home page only
    I have not modified any of your files. I have only added latest Javascript and latest home page

---
+++ script.js
@@ -0,0 +1,19 @@
+function button1_click(){
+window.open("add_sale.html");
+//alert("hai");
+}
+
+function button2_click()
+{
+ window.open("salesrecord.html");
+}
+
+function button3_click()
+{
+window.open("stocklist.html");
+}
+
+var button4 = document.getElementById("button4");
+button4.addEventListener("click",function(){
+	window.open("viewsalesreport.html");
+});
