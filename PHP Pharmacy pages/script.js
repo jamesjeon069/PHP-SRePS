2084a000848ec21de2728bf1c943d5b98ce3e376
102550264Brathaban <69609273+102550264Brathaban@users.noreply.github.com>
2021-05-10 00:58:01 +1000

    Javascript implementation Done--- all navigatiion buttons are functional
    Javascript implementation Done--- all navigation buttons are functional

--- PHP Pharmacy pages/script.js
+++ PHP Pharmacy pages/script.js
@@ -1,19 +1,78 @@
+
+
+                       //----------------- sales record page----------------------//
+
+var button1 = document.getElementById("addsales");
+button1.addEventListener("click",function(){
+	window.open("product_details.html");
+});
+
+
+var button2 = document.getElementById("salesrecords");
+button2.addEventListener("click",function(){
+	window.open("sales_records_page.html");
+});
+
+
+var button3 = document.getElementById("stocklist");
+button3.addEventListener("click",function(){
+	window.open("stock_list_page.html");
+});
+
+
+
+var button4 = document.getElementById("viewsalesreport");
+button4.addEventListener("click",function(){
+	window.open("viewsalesreport.html");
+});
+
+               
+
+
+               //----------------------Main page----------------//
+
 function button1_click(){
-window.open("addsales.html");
+window.open("product_details.html");
 //alert("hai");
 }
 
 function button2_click()
 {
- window.open("salesrecord.html");
+ window.open("sales_records_page.html");
 }
 
 function button3_click()
 {
-window.open("stocklist.html");
+window.open("stock_list_page.html");
 };
 
-var button4 = document.getElementById("button4");
-button4.addEventListener("click",function(){
-	window.open("viewsalesreport.html");
-});
+
+function button4_click()
+{
+window.open("viewsalesreport.html");
+};
+
+
+                  //----------------------stock page----------------//
+
+function button5_click(){
+window.open("product_details.html");
+//alert("hai");
+}
+
+function button6_click()
+{
+ window.open("sales_records_page.html");
+}
+
+function button7_click()
+{
+window.open("stock_list_page.html");
+};
+
+
+function button8_click()
+{
+window.open("viewsalesreport.html");
+};
+
